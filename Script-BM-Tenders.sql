f67f592452398cbff6b174545c883df8e95c9c07
rougerio <rougerio@yahoo.com>
2025-08-01 12:34:11 -0600

    ultima Mac

--- Script-BM-Tenders.sql
+++ Script-BM-Tenders.sql
@@ -270,6 +270,25 @@ and UTC.GET_LOCAL_DATE(s.start_time, ss.location_gid) between CAST(from_tz(cast(
 CAST(from_tz(cast(sysdate + 20 as timestamp), 'UTC') at time zone tz.time_zone_xid AS date)  
 and s.domain_name = 'NBL/MX'
 
+
+----BS_MISSED_PICKUPS_MX
+select s.shipment_gid 
+from shipment s inner join 
+shipment_stop ss on s.shipment_gid = ss.shipment_gid and ss.stop_num = 1 inner join 
+location loc on loc.location_gid = ss.location_gid inner join 
+time_zone tz on tz.time_zone_gid = loc.time_zone_gid 
+where (s.user_defined1_icon_gid IS NULL OR s.user_defined1_icon_gid <> 'NBL.SHIP_CONFIRMED') 
+and not exists ( select 1 
+                from shipment_remark sr 
+                where sr.shipment_gid = s.shipment_gid 
+                and remark_qual_gid = 'NBL.SHIP_DATE'
+                ) 
+and UTC.GET_LOCAL_DATE(s.start_time, ss.location_gid) > CAST(from_tz(cast(sysdate - 48/24 as timestamp), 'UTC') at time zone tz.time_zone_xid AS date) 
+and UTC.GET_LOCAL_DATE(s.start_time, ss.location_gid) < CAST(from_tz(cast(sysdate as timestamp), 'UTC') at time zone tz.time_zone_xid AS date) 
+and s.domain_name = 'NBL/MX
+
+
+
 /*
 TIMEOUT:
 
@@ -322,3 +341,7 @@ BS_SHIPMENTS_NO_APPOINTMENT-MX
 Embarques que no tienen Pickup appointment
 
 */
+
+
+rnolasco@niagarawater.com
+mariflores@niagarawater.com
