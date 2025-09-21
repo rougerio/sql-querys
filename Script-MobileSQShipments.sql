c4c6c3fb837e99c011a05c315e2fe6dd9df0aaa3
rougerio <rougerio@yahoo.com>
2025-09-21 10:38:17 -0600

    Nuevo

---
+++ Script-MobileSQShipments.sql
@@ -0,0 +1,26 @@
+select sh.shipment_gid
+from SHIPMENT sh, SHIPMENT_TYPE stp
+where (sh.shipment_gid=?)
+and (sh.perspective='B')
+and (sh.shipment_type_gid=stp.shipment_type_gid)
+and (stp.shipment_type_xid!='NON FREIGHT RELATED CHARGE')
+and (sh.domain_gid='NBL/MX')
+and (sh.shipment_gid in (select ss1.shipment_gid
+                        from SHIPMENT_STATUS ss1, STATUS_VALUE sv
+                        where (ss1.status_value_gid = sv.status_value_gid)
+                        and (sv.status_value_xid in ('ENROUTE_DELAYED','ENROUTE_DIVERTED','ENROUTE_ENROUTE','ENROUTE_MERGED','ENROUTE_PARTIAL','ENROUTE_UNLOADED - FULL','ENROUTE_UNLOADED - PARTIAL'))))
+order by sh.start_time desc
+
+
+
+select sh.shipment_gid
+from SHIPMENT sh, SHIPMENT_TYPE stp
+where (sh.perspective='B') 
+and (sh.shipment_type_gid=stp.shipment_type_gid) 
+and (stp.shipment_type_xid!='NON FREIGHT RELATED CHARGE') 
+and (sh.domain_gid='NBL/MX')
+and (sh.shipment_gid in (select ss1.shipment_gid
+                        from SHIPMENT_STATUS ss1, STATUS_VALUE sv
+                        where (ss1.status_value_gid = sv.status_value_gid) 
+                        and (sv.status_value_xid in ('ENROUTE_DELAYED','ENROUTE_DIVERTED','ENROUTE_ENROUTE','ENROUTE_MERGED','ENROUTE_PARTIAL','ENROUTE_UNLOADED - FULL','ENROUTE_UNLOADED - PARTIAL'))))
+order by sh.start_time desc
