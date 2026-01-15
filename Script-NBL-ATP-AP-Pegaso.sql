438503f86b0c7bf4c1513ad0454f353a01d3ac2c
rougerio <rougerio@yahoo.com>
2026-01-15 17:13:53 -0600

    15 Enero 2026

--- Script-NBL-ATP-AP-Pegaso.sql
+++ Script-NBL-ATP-AP-Pegaso.sql
@@ -6,7 +6,6 @@ select count(*)
   from nbl_ap_lin_pegaso_stg;
 
 select *
-  from nbl_ap_hdr_pegaso_stg hdr
- inner join nbl_ap_lin_pegaso_stg ln
-on hdr.record_id = ln.record_id
- order by hdr.record_id;
+from nbl_ap_hdr_pegaso_stg hdr inner join 
+nbl_ap_lin_pegaso_stg ln on hdr.record_id = ln.record_id
+order by hdr.record_id;
